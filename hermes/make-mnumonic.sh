d10bf6a2df39325e8ba555f7eeb98b0d6b54d8a4
Reece Williams <reecepbcups@gmail.com>
2022-11-21 14:09:05 -0600

    scripts

---
+++ hermes/make-mnumonic.sh
@@ -0,0 +1,3 @@
+read -p "Past your BIP 39 mnumonic: " mnumonic
+echo $mnumonic > relayer-mnemonic
+
---
+++ ibc-transfer.sh
@@ -0,0 +1,3 @@
+# Stargaze -> Joe
+
+starsd tx ibc-transfer transfer transfer channel-199 joe10r39fueph9fq7a6lgswu4zdsg8t3gxlq9c66u9 10000ustars --from relayer --node https://rpc.stargaze-apis.com:443 --chain-id stargaze-1 --fees 5000ustars --packet-timeout-height 0-0
