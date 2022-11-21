b5f80539dda91a9503b249bc92dc1aa8fd459dbf
Reece Williams <reecepbcups@gmail.com>
2022-11-21 14:27:45 -0600

    :( no 151

--- ibc-transfer.sh
+++ ibc-transfer.sh
@@ -1,3 +1,5 @@
-# Stargaze -> Joe
+# Stargaze relayer -> Joe Reece
 
-starsd tx ibc-transfer transfer transfer channel-199 joe10r39fueph9fq7a6lgswu4zdsg8t3gxlq9c66u9 10000ustars --from relayer --node https://rpc.stargaze-apis.com:443 --chain-id stargaze-1 --fees 5000ustars --packet-timeout-height 0-0
+# starsd tx ibc-transfer transfer transfer channel-151 joe10r39fueph9fq7a6lgswu4zdsg8t3gxlq9c66u9 10000ustars --from relayer --node https://rpc.stargaze-apis.com:443 --chain-id stargaze-1 --fees 5000ustars --packet-timeout-height 0-0
+
+starsd tx ibc-transfer transfer transfer channel-150 joe10r39fueph9fq7a6lgswu4zdsg8t3gxlq9c66u9 1ustars --from relayer --chain-id stargaze-1 --packet-timeout-height 0-0
